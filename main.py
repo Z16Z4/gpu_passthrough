d38c7f8c33e244763d514023134e2b3b1e81bbe0
cronos-hash <apollo@dnmx.org>
2021-02-06 05:18:54 +0000

    removed unnecessary lines

--- main.py
+++ main.py
@@ -10,29 +10,20 @@ if cpu == "amd":
     iommu_type = '"amd_iommu=on iommu=pt '
 elif cpu == "intel":
     iommu_type = '"intel_iommu=on iommu=pt '
-
 #Opens grub file, puts each line into an array
 with open('grub') as grubfile:
     grubline = [i.strip() for i in grubfile]
-
-
-#this is the line needing editing
-print(grubline[5])
 #looping through array
 for y in grubline:
     #matching needed line with looped array
     x = re.search('^GRUB_CMDLINE_LINUX_DEFAULT=".*"', y)
     if x:
         #if syntax is correct then stored needed string in editedgrub
-        print("Yes")
+        print("Edited grubline")
         #replace each '"' occurance in grubline array with iommu
         editedgrub = re.sub('"', iommu_type, y, 1)
         print(editedgrub)
         for line in fileinput.FileInput('grub', inplace=1):
             line=line.replace(y, editedgrub)
             print(line.strip())
-
-        else:
-            #if the syntax is not correct then
-            print("No match")
 
