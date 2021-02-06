5749bafe0fd1b9070cc07d24953ec83d9fee0a91
cronos-hash <apollo@dnmx.org>
2021-02-06 05:04:08 +0000

    writes changes to grub file from array

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ print("Enter intel or amd depending on your cpu make")
 cpu = input("Please enter your cpu brand:")
 file_name = 'grub' #this will be /etc/default/grub once deployed
 if cpu == "amd":
-    iommu_type = "amd_iommu=on iommu=pt "
+    iommu_type = '"amd_iommu=on iommu=pt '
 elif cpu == "intel":
-    iommu_type = "intel_iommu=on iommu=pt "
+    iommu_type = '"intel_iommu=on iommu=pt '
 
 #Opens grub file, puts each line into an array
 with open('grub') as grubfile:
@@ -18,16 +18,21 @@ with open('grub') as grubfile:
 
 #this is the line needing editing
 print(grubline[5])
+#looping through array
+for y in grubline:
+    #matching needed line with looped array
+    x = re.search('^GRUB_CMDLINE_LINUX_DEFAULT=".*"', y)
+    if x:
+        #if syntax is correct then stored needed string in editedgrub
+        print("Yes")
+        #replace each '"' occurance in grubline array with iommu
+        editedgrub = re.sub('"', iommu_type, y, 1)
+        print(editedgrub)
+        for line in fileinput.FileInput('grub', inplace=1):
+            line=line.replace(y, editedgrub)
+            print(line.strip())
 
-#matching line to make sure the syntax is correct
-x = re.search('^GRUB_CMDLINE_LINUX_DEFAULT=".*"', grubline[5])
-if x:
-    #if syntax is correct then
-    print("Yes")
-    #replace each '"' occurance in grubline array with iommu
-    editgrubbuff = re.sub('"', iommu_type, grubline[5], 1)
-    print(editgrubbuff)
-else:
-    #if the syntax is not correct then
-    print("No match")
+        else:
+            #if the syntax is not correct then
+            print("No match")
 
