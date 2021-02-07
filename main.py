80bd56867c9a7d7396505a69fc51bbd71aa1f0c9
cronos-hash <apollo@dnmx.org>
2021-02-07 10:52:00 +0000

    kernal modules, error in arrayline

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import os
 from sys import platform
 print("Enter intel or amd depending on your cpu make")
 cpu = input("Please enter your cpu brand:")
-file_name = 'grub' #this will be /etc/default/grub once deployed
+file_name = 'examplegrub' #this will be /etc/default/grub once deployed
 if cpu == "amd":
     iommu_type = '"amd_iommu=on iommu=pt '
 elif cpu == "intel":
     iommu_type = '"intel_iommu=on iommu=pt '
 #Opens grub file, puts each line into an array
-with open('grub') as grubfile:
+with open('examplegrub') as grubfile:
     grubline = [i.strip() for i in grubfile]
 #looping through array
 for y in grubline:
@@ -25,7 +25,7 @@ for y in grubline:
         #replace each '"' occurance in grubline array with iommu
         editedgrub = re.sub('"', iommu_type, y, 1)
         print(editedgrub)
-        for line in fileinput.FileInput('grub', inplace=1):
+        for line in fileinput.FileInput('examplegrub', inplace=1):
             line=line.replace(y, editedgrub)
             print(line.strip())
 system = input("please enter your system (linux/arch)")
@@ -36,11 +36,11 @@ if system == "linux":
     os.system(grubupdate)
 elif system == "arch":
     os.system(grubupdate)
-    with open('mkinitcpio.conf') as loadfile:
-        loadline = [i.strip() for i in loadline]
+    with open('example.conf') as loadfile:
+        arrayline = [i.strip() for i in arrayline]
     for it in loadline:
         x = re.search('^MODULES=(".*"', it)
-    if syntax_correct:
+    if x:
         print("Editing kernal modules")
         kernal_modules = re.sub('"', vfio, it, 1)
         print(kernal_modules)
